f8e21af2c7f400016d496c3c46365bc2a1c7b936
Andrew Wagenmaker <ajwagen@cs.washington.edu>
2020-04-22 14:49:50 -0700

    Initial Commit

---
+++ gesture_sensor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gesture_sensor
+//
+//  Created by Andrew Wagenmaker on 4/22/20.
+//  Copyright © 2020 Andrew Wagenmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
