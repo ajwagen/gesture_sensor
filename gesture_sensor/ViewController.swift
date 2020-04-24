02ed147a1f98afdff9ed17c897aa35ae5a5948fe
Andrew Wagenmaker <ajwagen@cs.washington.edu>
2020-04-24 08:38:47 -0700

    Updating app

--- gesture_sensor/ViewController.swift
+++ gesture_sensor/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIApplicationDelegate, AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var gestureView: GestureView!
+  
+    var audioInput: TempiAudioInput!
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let audioInputCallback: TempiAudioInputCallback = { (timeStamp, numberOfFrames, samples) -> Void in
+            self.gotSomeAudio(timeStamp: Double(timeStamp), numberOfFrames: Int(numberOfFrames), samples: samples)
+        }
+        
+        let test_sound = Bundle.main.path(forResource: "tone_18khz", ofType: "wav")
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: test_sound!))
+            audioPlayer.delegate = self
+        
+        } catch {
+            
+        }
+        
+        audioPlayer.play()
+        
+        audioInput = TempiAudioInput(audioInputCallback: audioInputCallback, sampleRate: 44100, numberOfChannels: 1)
+        audioInput.startRecording()
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        audioPlayer.play()
     }
 
+    func gotSomeAudio(timeStamp: Double, numberOfFrames: Int, samples: [Float]) {
+        let fft = TempiFFT(withSize: 512, sampleRate: 44100.0)
+        fft.windowType = TempiFFTWindowType.hamming
+        fft.fftForward(samples)
 
+        tempi_dispatch_main { () -> () in
+            self.gestureView.fft = fft
+            self.gestureView.setNeedsDisplay()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        NSLog("*** Memory!")
+        super.didReceiveMemoryWarning()
+    }
 }
+
 
